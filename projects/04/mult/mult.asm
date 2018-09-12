222c600c9ee2e89f07b93ccfab13ae325672db84
Mikael Engström <mikael.pj.engstrom@gmail.com>
2018-09-12 14:40:03 +0200

    Implement Multiplication in HACK-asm

--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -6,4 +6,73 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-// Put your code here.
+// for n R0:
+//   R2 = R2 + R1
+// exit
+
+// Clean up from previos run
+@R2
+M=0
+
+// Setup variables
+@i
+M=0
+
+// Assert not negative, will be overriden if thats the case
+@isneg
+M=0
+
+// Calculate number of iterations and set to N
+@R0
+D=M
+
+@POS
+D; JGE
+
+@isneg
+M=-1
+D=!D
+D=D+1
+(POS)
+  @n
+  M=D
+
+(LOOP)
+  // if n - i is 0: break;
+  @n
+  D=M
+  @i
+  D=D-M
+
+  @BREAK
+  D;JEQ
+
+  @R1
+  D=M
+  @R2
+  M=D+M
+
+  // i++
+  @i
+  M=M+1
+
+  @LOOP
+  0;JMP
+
+(BREAK)
+  // if R0 is possitive, goto END
+  @isneg
+  D=M
+  @END
+  D; JEQ
+
+  // Invert R2
+  @R2
+  D=!M
+  M=D+1
+
+
+(END)
+  @END
+  0;JMP
+
