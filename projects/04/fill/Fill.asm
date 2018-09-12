c8dc26c27cdc907b5db8ecd22582b130f546f9dd
Mikael Engström <mikael.pj.engstrom@gmail.com>
2018-09-12 17:41:28 +0200

    Finish the fill-application

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -11,4 +11,90 @@
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
+// This program excpect R0 to be set to 8160 befor starting (the whole range of
+// the screen module)
+
+// Set variables
+   @SCREEN
+   D=A
+   @address
+   M=D
+
+// Get number of registers that the screen-module contains from R0
+   @R0
+   D=M
+   @length
+   M=D
+
+(LOOP)
+   @KBD
+   D=M
+   @lastinput
+   M=D
+
+(LISTEN)
+   @lastinput
+   D=M
+   @KBD
+   D=D-M
+   @LISTEN
+   D; JEQ // input did change
+// On change, jump to RELEASE on keyup
+   @KBD
+   D=M
+   @DRAWWHITE
+   D; JEQ
+   @DRAWBLACK
+   0; JMP
+
+(DRAWWHITE)
+   @color
+   M=0
+   @DRAW
+   0; JMP
+
+(DRAWBLACK)
+   @color
+   M=-1
+   @DRAW
+   0; JMP
+
+(DRAW)
+   @i
+   M=0
+   (WRITE)
+   // Get address
+      @SCREEN
+      D=A
+      @i
+      D=D+M
+      @address
+      M=D
+
+   // if screen is full: break;
+      @length
+      D=M
+      @i
+      D=D-M
+      @ENDWRITE
+      D; JEQ
+
+   // Set color
+      @color
+      D=M
+
+   // Write
+      @address
+      A=M
+      M=D
+
+   // Increment iterator
+      @i
+      M=M+1
+
+      @WRITE
+      0; JMP
+
+   (ENDWRITE)
+     @LOOP
+     0; JMP
