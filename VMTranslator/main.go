c6efa42f7ca8f63cae842715a658d9815350a885
Mikael Engström <mikael.pj.engstrom@gmail.com>
2018-09-16 17:31:10 +0200

    Restructure project

---
+++ VMTranslator/main.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"bufio"
+	"os"
+	"path/filepath"
+	"regexp"
+)
+
+func main() {
+	fileNameOrDirectory := os.Args[1]
+
+	file, err := os.Stat(fileNameOrDirectory)
+	if err != nil {
+		panic(err)
+	}
+
+	inFiles := []string{fileNameOrDirectory}
+
+	re := regexp.MustCompile("\\..+$")
+	outputFilename := re.ReplaceAllString(fileNameOrDirectory, "") + ".asm"
+	if file.Mode().IsDir() {
+		inFiles, err = filepath.Glob(filepath.Join(fileNameOrDirectory, "*.vm"))
+		if err != nil {
+			panic(err)
+		}
+
+		dirName := filepath.Base(fileNameOrDirectory)
+		outputFilename = filepath.Join(fileNameOrDirectory, dirName + ".asm")
+	}
+
+
+	outfile := createFile(outputFilename)
+	defer outfile.Close()
+
+	var instructionIndex int;
+	for _, infilePath := range inFiles {
+		infile := openFile(infilePath)
+		defer infile.Close()
+
+		_, fileName := filepath.Split(infilePath)
+		codeWriterPrefix := re.ReplaceAllString(fileName, "")
+
+		scanner := bufio.NewScanner(bufio.NewReader(infile))
+		parser := NewParser(scanner)
+
+		for {
+			instruction := parser.NextInstruction()
+			if instruction.command == CommandTerminate {
+				break
+			}
+
+			instructionIndex++
+
+			asmInstructions := translate(instruction, codeWriterPrefix)
+			for _, row := range asmInstructions {
+				row = insertJumpIndexes(row, instructionIndex)
+				outfile.WriteString(row + "\n")
+			}
+
+		}
+	}
+}
+
+
+func openFile(filename string) *os.File{
+	file, err := os.Open(filename)
+	if err != nil {
+		panic(err)
+	}
+
+	return file
+}
+
+func createFile(filename string) *os.File{
+	file, err := os.Create(filename)
+	if err != nil {
+		panic(err)
+	}
+
+	return file
+}
