c530e8b7c4286d2bb8fca888cdb39bd0071f604b
mien <mikael.pj.engstrom@gmail.com>
2018-09-30 20:57:12 +0200

    Refactored Parser

--- JACKCompiler/CompilationEngine.py
+++ JACKCompiler/CompilationEngine.py
@@ -1,3 +1,4 @@
+from functools import partial
 from collections import namedtuple
 from Tokenizer import TokenizerReachedEndOfFileException, Token
 
@@ -11,34 +12,42 @@ class CompilationEngine:
         self.tokenizer = tokenizer
 
     def compile(self):
-        token = self.tokenizer.advance()
-        if token.type != 'keyword' and token.name != 'class':
-            raise Exception("JACK programs must start with a keyword class")
-
         return SyntaxNode(name='class', value=self.compile_class())
 
+    def _advance_token(self, assertion=None):
+        token = self.tokenizer.advance()
+        if assertion:
+            assertion(token)
+
+        return token
+
+    def _advance_node(self, assertion=None):
+        """Move the tokenizer cursor and cast the result to SyntaxNode"""
+        return SyntaxNode.from_token(self._advance_token(assertion))
+
+    # Class declarations
+
     def compile_class(self):
-        _r = [SyntaxNode(name='keyword', value='class')]
-
-        token = self.tokenizer.advance()
-        _assert_identifier(token)
-
-        _r.append(SyntaxNode(name='identifier', value=token.token))
-
-        token = self.tokenizer.advance()
-        _assert_symbol('{', token)
-        _r.append(SyntaxNode.from_token(token))
+        _r = [
+            self._advance_node(partial(_assert_token, 'class')),
+            self._advance_node(_assert_identifier),
+            self._advance_node(partial(_assert_symbol, '{')),
+        ]
 
         while True:
             try:
                 token = self.tokenizer.peak_next()
+
                 if token.token == '}':
-                    _r.append(SyntaxNode.from_token(self.tokenizer.advance()))
+                    _r.append(self._advance_node())
                     return _r
+
                 elif token.token in ['field', 'static']:
                     _r.append(self.compile_class_var_dec())
+
                 elif token.token in ['constructor', 'function', 'method']:
                     _r.append(self.compile_subroutine_dec())
+
                 else:
                     raise Exception("Unexpected token '{}'".format(token.token))
 
@@ -46,16 +55,13 @@ class CompilationEngine:
                 raise Exception("Expected '}' for class")
 
     def compile_class_var_dec(self):
-        _r = [SyntaxNode.from_token(self.tokenizer.advance())]
+        _r = [
+            self._advance_node(),
+            self._advance_node(_assert_type),
+            self._advance_node(_assert_identifier),
+        ]
 
-        token = self.tokenizer.advance()
-        _assert_type(token)
-        _r.append(SyntaxNode.from_token(token))
-
-        token = self.tokenizer.advance()
-        _assert_identifier(token)
-        _r.append(SyntaxNode.from_token(token))
-
+        token = self.tokenizer.last_token
         while True:
             last_token = token
             token = self.tokenizer.advance()
@@ -69,30 +75,18 @@ class CompilationEngine:
             else:
                 _assert_identifier(token)
 
+    # Subroutines
+
     def compile_subroutine_dec(self):
-        _r = [SyntaxNode.from_token(self.tokenizer.advance())]
-
-        token = self.tokenizer.advance()
-        _assert_type(token)
-        _r.append(SyntaxNode(name=token.type, value=token.token))
-
-        token = self.tokenizer.advance()
-        _assert_identifier(token)
-        _r.append(SyntaxNode(name=token.type, value=token.token))
-
-        token = self.tokenizer.advance()
-        _assert_symbol('(', token)
-        _r.append(SyntaxNode(name=token.type, value=token.token))
-
-        _r.append(self.compile_parameter_list())
-
-        token = self.tokenizer.advance()
-        _assert_symbol(')', token)
-        _r.append(SyntaxNode.from_token(token))
-
-        _r.append(self.compile_subroutine_body())
-
-        return SyntaxNode(name='subroutineDec', value=_r)
+        return SyntaxNode(name='subroutineDec', value=[
+            self._advance_node(),
+            self._advance_node(_assert_type),
+            self._advance_node(_assert_identifier),
+            self._advance_node(partial(_assert_symbol, '(')),
+            self.compile_parameter_list(),
+            self._advance_node(partial(_assert_symbol, ')')),
+            self.compile_subroutine_body(),
+        ])
 
     def compile_parameter_list(self):
         _r = []
@@ -102,65 +96,45 @@ class CompilationEngine:
             if next_token.token == ')':
                 return SyntaxNode(name="parameterList", value=_r)
             elif next_token.token == ',':
-                _r.append(SyntaxNode.from_token(self.tokenizer.advance()))
+                _r.append(self._advance_node())
                 continue
 
             _r += self.compile_parameter()
 
     def compile_parameter(self):
-        _r = []
-
-        token = self.tokenizer.advance()
-        _assert_type(token)
-        _r.append(SyntaxNode.from_token(token))
-
-        token = self.tokenizer.advance()
-        _assert_identifier(token)
-        _r.append(SyntaxNode.from_token(token))
-
-        return _r
+        return [
+            self._advance_node(_assert_type),
+            self._advance_node(_assert_identifier),
+        ]
 
     def compile_subroutine_body(self):
-        _r = []
-
-        token = self.tokenizer.advance()
-        _assert_symbol('{', token)
-        _r.append(SyntaxNode.from_token(token))
+        _r = [
+            self._advance_node(partial(_assert_symbol, '{'))
+        ]
 
         while self.tokenizer.peak_next().token == 'var':
             _r.append(self.compile_var_dec())
 
         _r.append(self.compile_statements())
-
-        token = self.tokenizer.advance()
-        _assert_symbol('}', token)
-        _r.append(SyntaxNode.from_token(token))
+        _r.append(self._advance_node(partial(_assert_symbol, '}')))
 
         return SyntaxNode(name="subroutineBody", value=_r)
 
     def compile_var_dec(self):
-        _r = []
-
-        token = self.tokenizer.advance()
-        _assert_token('var', token)
-        _r.append(SyntaxNode.from_token(token))
-
-        token = self.tokenizer.advance()
-        _assert_type(token)
-        _r.append(SyntaxNode.from_token(token))
-
-        token = self.tokenizer.advance()
-        _assert_identifier(token)
-        _r.append(SyntaxNode.from_token(token))
+        _r = [
+            self._advance_node(partial(_assert_token, 'var')),
+            self._advance_node(_assert_type),
+            self._advance_node(_assert_identifier),
+        ]
 
         while self.tokenizer.peak_next().token != ';':
-            _r.append(SyntaxNode.from_token(self.tokenizer.advance()))
+            _r.append(self._advance_node())
 
-        token = self.tokenizer.advance()
-        _assert_symbol(';', token)
-        _r.append(SyntaxNode.from_token(token))
+        _r.append(self._advance_node(partial(_assert_symbol, ';')))
 
         return SyntaxNode(name="varDec", value=_r)
+
+    # Statements
 
     def compile_statements(self):
         _r = []
@@ -186,27 +160,93 @@ class CompilationEngine:
                 _r.append(self.compile_while_statement())
 
             else:
-                raise Exception('subroutine: NOTIMPLEMENTED: {}'.format(token))
+                raise Exception('Unexpected statement: {}'.format(token))
 
     def compile_return_statement(self):
-        _r = [SyntaxNode.from_token(self.tokenizer.advance())]
+        _r = [self._advance_node()]
 
         if self.tokenizer.peak_next().token == ';':
-            _r.append(SyntaxNode.from_token(self.tokenizer.advance()))
+            _r.append(self._advance_node())
         else:
             _r.append(self.compile_expression())
 
-            token = self.tokenizer.advance()
-            _assert_symbol(';', token)
-            _r.append(SyntaxNode.from_token(token))
+            _r.append(self._advance_node(partial(_assert_symbol, ';')))
 
         return SyntaxNode(name='returnStatement', value=_r)
+
+    def compile_let_statement(self):
+        _r = [
+            self._advance_node(),
+            self._advance_node(_assert_identifier),
+        ]
+
+        if self.tokenizer.peak_next().token == '[':
+            _r.append(self._advance_node(partial(_assert_symbol, '[')))
+            _r.append(self.compile_expression())
+            _r.append(self._advance_node(partial(_assert_symbol, ']')))
+
+        _r.append(self._advance_node(partial(_assert_symbol, '=')))
+
+        if self.tokenizer.peak_next().token != ';':
+            _r.append(self.compile_expression())
+
+        _r.append(self._advance_node(partial(_assert_symbol, ';')))
+
+        return SyntaxNode(name='letStatement', value=_r)
+
+    def compile_do_statement(self):
+        return SyntaxNode(name='doStatement', value=[
+            self._advance_node(),
+            *self.compile_subroutine_call(),
+            self._advance_node(partial(_assert_symbol, ';')),
+        ])
+
+    def compile_if_statement(self):
+        _r = [
+            self._advance_node(),
+            self._advance_node(partial(_assert_symbol, '(')),
+            self.compile_expression(),
+            self._advance_node(partial(_assert_symbol, ')')),
+            self._advance_node(partial(_assert_symbol, '{')),
+            self.compile_statements(),
+            self._advance_node(partial(_assert_symbol, '}'))
+        ]
+
+        if self.tokenizer.peak_next().token == 'else':
+            _r.append(self._advance_node())
+            _r.append(self._advance_node(partial(_assert_symbol, '{')))
+            _r.append(self.compile_statements())
+            _r.append(self._advance_node(partial(_assert_symbol, '}')))
+
+        return SyntaxNode(name='ifStatement', value=_r)
+
+    def compile_while_statement(self):
+        return SyntaxNode(name='whileStatement', value=[
+            self._advance_node(),
+            self._advance_node(partial(_assert_symbol, '(')),
+            self.compile_expression(),
+            self._advance_node(partial(_assert_symbol, ')')),
+            self._advance_node(partial(_assert_symbol, '{')),
+            self.compile_statements(),
+            self._advance_node(partial(_assert_symbol, '}')),
+        ])
+
+    # Expressions
+
+    def compile_expression_list(self):
+        _r = []
+        while self.tokenizer.peak_next().token != ')':
+            _r.append(self.compile_expression())
+            if self.tokenizer.peak_next().token == ',':
+                _r.append(self._advance_node())
+
+        return SyntaxNode(name='expressionList', value=_r)
 
     def compile_expression(self):
         _r = [self.compile_term()]
 
-        while self._is_op(self.tokenizer.peak_next()):
-            _r.append(SyntaxNode.from_token(self.tokenizer.advance()))
+        while _is_op(self.tokenizer.peak_next()):
+            _r.append(self._advance_node())
             _r.append(self.compile_term())
 
         return SyntaxNode(name='expression', value=_r)
@@ -223,168 +263,43 @@ class CompilationEngine:
 
         _r.append(SyntaxNode.from_token(token))
 
-        if self._is_unary_op(token):
+        if _is_unary_op(token):
             _r.append(self.compile_term())
 
         elif token.token == '(':
             _r.append(self.compile_expression())
-
-            token = self.tokenizer.advance()
-            _assert_symbol(')', token)
-            _r.append(SyntaxNode.from_token(token))
+            _r.append(self._advance_node(partial(_assert_symbol, ')')))
 
         elif next_token.token == '(':
             _r += self.compile_subroutine_call()
 
         elif next_token.token == '[':
-            _r.append(SyntaxNode.from_token(self.tokenizer.advance()))
-
+            _r.append(self._advance_node())
             _r.append(self.compile_expression())
-
-            token = self.tokenizer.advance()
-            _assert_symbol(']', token)
-            _r.append(SyntaxNode.from_token(token))
+            _r.append(self._advance_node(partial(_assert_symbol, ']')))
 
         return SyntaxNode(name='term', value=_r)
 
-    def _is_op(self, token):
-        return token.token in ['+', '-', '*', '/', '&', '|', '<', '>', '=']
-
-    def _is_unary_op(self, token):
-        return token.token in ['-', '~']
-
-    def compile_let_statement(self):
-        _r = [SyntaxNode.from_token(self.tokenizer.advance())]
-
-        token = self.tokenizer.advance()
-        _assert_identifier(token)
-        _r.append(SyntaxNode.from_token(token))
-
-        if self.tokenizer.peak_next().token == '[':
-            token = self.tokenizer.advance()
-            _assert_symbol('[', token)
-            _r.append(SyntaxNode.from_token(token))
-
-            _r.append(self.compile_expression())
-
-            token = self.tokenizer.advance()
-            _assert_symbol(']', token)
-            _r.append(SyntaxNode.from_token(token))
-
-        token = self.tokenizer.advance()
-        _assert_symbol('=', token)
-        _r.append(SyntaxNode.from_token(token))
-
-        if self.tokenizer.peak_next().token != ';':
-            _r.append(self.compile_expression())
-
-        token = self.tokenizer.advance()
-        _assert_symbol(';', token)
-        _r.append(SyntaxNode.from_token(token))
-
-        return SyntaxNode(name='letStatement', value=_r)
-
-    def compile_do_statement(self):
-        _r = [SyntaxNode.from_token(self.tokenizer.advance())]
-
-        _r += self.compile_subroutine_call()
-
-        token = self.tokenizer.advance()
-        _assert_symbol(';', token)
-        _r.append(SyntaxNode.from_token(token))
-
-        return SyntaxNode(name='doStatement', value=_r)
+    # Etc
 
     def compile_subroutine_call(self):
         _r = []
 
         while self.tokenizer.peak_next().token != '(':
-            token = self.tokenizer.advance()
-            _r.append(SyntaxNode.from_token(token))
+            _r.append(self._advance_node())
 
-        token = self.tokenizer.advance()
-        _assert_symbol('(', token)
-        _r.append(SyntaxNode.from_token(token))
-
+        _r.append(self._advance_node(partial(_assert_symbol, '(')))
         _r.append(self.compile_expression_list())
-
-        token = self.tokenizer.advance()
-        _assert_symbol(')', token)
-        _r.append(SyntaxNode.from_token(token))
+        _r.append(self._advance_node(partial(_assert_symbol, ')')))
 
         return _r
 
-    def compile_expression_list(self):
-        _r = []
-        while self.tokenizer.peak_next().token != ')':
-            _r.append(self.compile_expression())
-            if self.tokenizer.peak_next().token == ',':
-                _r.append(SyntaxNode.from_token(self.tokenizer.advance()))
+def _is_op(token):
+    return token.token in ['+', '-', '*', '/', '&', '|', '<', '>', '=']
 
-        return SyntaxNode(name='expressionList', value=_r)
 
-    def compile_if_statement(self):
-        _r = [SyntaxNode.from_token(self.tokenizer.advance())]
-
-        token = self.tokenizer.advance()
-        _assert_symbol('(', token)
-        _r.append(SyntaxNode.from_token(token))
-
-        _r.append(self.compile_expression())
-
-        token = self.tokenizer.advance()
-        _assert_symbol(')', token)
-        _r.append(SyntaxNode.from_token(token))
-
-        token = self.tokenizer.advance()
-        _assert_symbol('{', token)
-        _r.append(SyntaxNode.from_token(token))
-
-        _r.append(self.compile_statements())
-
-        token = self.tokenizer.advance()
-        _assert_symbol('}', token)
-        _r.append(SyntaxNode.from_token(token))
-
-        if self.tokenizer.peak_next().token == 'else':
-            _r.append(SyntaxNode.from_token(self.tokenizer.advance()))
-
-            token = self.tokenizer.advance()
-            _assert_symbol('{', token)
-            _r.append(SyntaxNode.from_token(token))
-
-            _r.append(self.compile_statements())
-
-            token = self.tokenizer.advance()
-            _assert_symbol('}', token)
-            _r.append(SyntaxNode.from_token(token))
-
-        return SyntaxNode(name='ifStatement', value=_r)
-
-    def compile_while_statement(self):
-        _r = [SyntaxNode.from_token(self.tokenizer.advance())]
-
-        token = self.tokenizer.advance()
-        _assert_symbol('(', token)
-        _r.append(SyntaxNode.from_token(token))
-
-        _r.append(self.compile_expression())
-
-        token = self.tokenizer.advance()
-        _assert_symbol(')', token)
-        _r.append(SyntaxNode.from_token(token))
-
-        token = self.tokenizer.advance()
-        _assert_symbol('{', token)
-        _r.append(SyntaxNode.from_token(token))
-
-        _r.append(self.compile_statements())
-
-        token = self.tokenizer.advance()
-        _assert_symbol('}', token)
-        _r.append(SyntaxNode.from_token(token))
-
-        return SyntaxNode(name='whileStatement', value=_r)
+def _is_unary_op(token):
+    return token.token in ['-', '~']
 
 
 def _assert_type(token):
